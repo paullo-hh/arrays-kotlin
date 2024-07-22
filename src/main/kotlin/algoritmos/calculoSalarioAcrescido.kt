4db92f38171436cb1b6f4be8e3193ba841195099
Paulo H. Carvalho <contacthenry96@gmail.com>
2024-07-22 11:02:33 -0300

    Adjustment in the logic of the calculation function

--- src/main/kotlin/algoritmos/calculoSalarioAcrescido.kt
+++ src/main/kotlin/algoritmos/calculoSalarioAcrescido.kt
@@ -1,21 +1,24 @@
 package algoritmos
 
 fun main() {
+    println("Um salário superior aproximado seria: ${"R$"} ${calculo()}")
+}
 
-    println("Vamos calcular um possível aumento de salário:")
+fun calculo(): String {
+    println("Vamos calcular um possível aumento de salário")
 
     println("Digite sua faixa salarial atual: ")
-    var salarioAtual = readln().toFloat()
+    var salarioAtual: Float = readln().toFloat()
 
-    var salarioMinimo = 1400.00
-    var salario = 0.00
+    var salarioMinimo: Float = 1400.0F
+    var salario: Float = 0.0F
 
     if (salarioAtual <= salarioMinimo) {
-        salario = salarioAtual * 1.30
+        salario = salarioAtual * 1.30.toFloat()
     } else {
-        salario = salarioAtual * 1.10
+        salario = salarioAtual * 1.10.toFloat()
     }
 
     var salarioFormatado = String.format("%.2f", salario)
-    println("Um salário superior aproximado seria: ${"R$"} $salarioFormatado")
+    return salarioFormatado
 }
